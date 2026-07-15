4cac97e51749a6ea0f08715b506f32c32a6525a1
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-07-15 13:57:22 -0300

    15/07/2026 - Created: Content in /lib; files for the layout in /components

---
+++ ecommerce/src/lib/fetcher.ts
@@ -0,0 +1,9 @@
+export async function apiFetch(path: string, options: RequestInit = {}) {
+  return fetch(path, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers
+    }
+  })
+}
