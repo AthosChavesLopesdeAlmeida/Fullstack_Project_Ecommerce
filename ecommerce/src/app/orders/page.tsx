2b70f2715bb13afd58647daf008d6bb73f9dfca2
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-07-19 13:38:08 -0300

    19/07/2026 - Fixed: Router.push to /orders/[id]; Tested the application and everything seems to be working just fine :)

--- ecommerce/src/app/orders/page.tsx
+++ ecommerce/src/app/orders/page.tsx
@@ -19,6 +19,10 @@ const Page = () => {
     setOrders(data)
   }
 
+  const redirect = (id: string) => {
+    router.push(`/orders/${id}`)
+  }
+
   useEffect(() => {
     fetchOrders()
   }, [])
@@ -32,7 +36,8 @@ const Page = () => {
       <div className="grid grid-cols-3 gap-6">
         {orders.map((order: Order) => (
           <div key={order.id}
-          className="bg-[#5C4033] p-4 rounded-lg shadow-lg border border-[#D9CBB8] flex flex-col gap-2">
+          className="bg-[#5C4033] p-4 rounded-lg shadow-lg border border-[#D9CBB8] flex flex-col gap-2"
+          onClick={() => redirect(order.id)}>
 
             <h5 className="text-lg font-bold text-[#F5F1E8] mt-2">{new Date(order.createdAt).toLocaleDateString()}</h5>
             <p className="text-sm text-[#D9CBB8]">{order.status}</p>
